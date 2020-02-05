c1e63e98e85efbe621df5a8bd318b397125587c3
Andy H. Jung <commaniakr@gmail.com>
2020-02-05 13:59:46 +0900

    test: containsOrNot 테스트 추가

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -2,8 +2,10 @@ package study;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -21,14 +23,23 @@ public class SetTest {
     }
 
     @Test
+    @DisplayName("Set의 사이즈를 확인하는 테스트")
     void size() {
         int size = numbers.size();
         Assertions.assertThat(size).isEqualTo(3);
     }
 
     @ParameterizedTest
+    @DisplayName("값이 포함되었는지 확인하는 테스트")
     @ValueSource(ints = { 1,2,3 })
     void contains(Integer number) {
         Assertions.assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("값이 포함되어 있는지 아닌지 확인하는 테스트")
+    @CsvSource(value = { "1:true", "2:true", "3:true", "5:false"}, delimiter = ':')
+    void containsOrNot(String input, String value) {
+        Assertions.assertThat(numbers.contains(Integer.parseInt(input))).isEqualTo(Boolean.parseBoolean(value));
     }
 }
