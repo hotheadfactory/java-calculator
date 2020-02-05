a629bb06b1f25062411d9adaac6c3983adc78352
Andy H. Jung <commaniakr@gmail.com>
2020-02-05 14:54:45 +0900

    feat: Calculator 클래스 추가

--- src/main/java/study/Calculator.java
+++ src/main/java/study/Calculator.java
@@ -1,5 +1,33 @@
 package study;
 
-public class Calculator {
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class Calculator {
+    public static float calculate(String[] values) {
+        float result = 0;
+        Queue<String> elements = new LinkedList<>(Arrays.asList(values));
+        result = Float.parseFloat(elements.poll());
+        while (!elements.isEmpty()) {
+            String operator = elements.poll();
+            float operand = Float.parseFloat(elements.poll());
+            result = calculateEach(result, operator, operand);
+        }
+        return result;
+    }
+
+    private static float calculateEach(float result, String operator, float operand) {
+        switch (operator) {
+            case "+":
+                return result + operand;
+            case "-":
+                return result - operand;
+            case "/":
+                return result / operand;
+            case "*":
+                return result * operand;
+        }
+        throw new IllegalArgumentException("올바르지 않은 계산식입니다!");
+    }
 }
--- src/test/java/study/CalculatorTest.java
+++ src/test/java/study/CalculatorTest.java
@@ -1,13 +1,18 @@
 package study;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CalculatorTest {
-    @Test
-    void calculate() {
-        String value = "2 + 3 * 4 / 2";
-        String[] values = value.split(" ");
-        Assertions.assertThat(Calculator.calculate(values)).isEqualTo(10);
+    @ParameterizedTest
+    @DisplayName("값이 포함되었는지 확인하는 테스트")
+    @CsvSource(value = { "2 + 3 * 4 / 2:10", "4 + 5 / 2 + 7:11.5"}, delimiter = ':')
+    void calculate(String expression, String answer) {
+        String[] values = expression.split(" ");
+        Assertions.assertThat(Calculator.calculate(values)).isEqualTo(Float.parseFloat(answer));
     }
 }
