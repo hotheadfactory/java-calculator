dedacd425288cd6002cd530f3212c269ff90afb4
Andy H. Jung <commaniakr@gmail.com>
2020-02-05 13:50:42 +0900

    test: contains 테스트 추가

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,8 @@ package study;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -22,5 +24,11 @@ public class SetTest {
     void size() {
         int size = numbers.size();
         Assertions.assertThat(size).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 1,2,3 })
+    void contains(Integer number) {
+        Assertions.assertThat(numbers.contains(number)).isTrue();
     }
 }
